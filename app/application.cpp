45f8fba4519de37e6bd48c8ab083d57be44a1ab2
Vyacheslav <v.semenov@t-ener.com>
2024-05-19 21:42:54 +0300

    problem fixing

--- app/application.cpp
+++ app/application.cpp
@@ -16,12 +16,11 @@ void PrintLcdText()
 	LL_GPIO_SetOutputPin(LED_GPIO_Port, LED_Pin);
 	Lcd1309* lcd_display = new Lcd1309;
 	lcd_display->Clear();
-	//ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	lcd_display->Print("Andrei Fursenko: First of all, in terms of the transition to a new education system.");
 }
 
 void Application()
 {
-	LL_Init1msTick(SysTick_CTRL_COUNTFLAG_Msk);
+	LL_Init1msTick(SysTick_LOAD_RELOAD_Pos);
 	PrintLcdText();
 }
